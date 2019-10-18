9bb35197219ca7d6d634bd3190cd745f4f2e5fe2
melodyshi <melodyshi@tencent.com>
2019-10-18 18:33:25 +0800

    init

---
+++ logShow.js
@@ -0,0 +1,42 @@
+/*
+日志协议：
+[{
+      "appName": "认证系统",
+      "equipmentName": "ykserver172",
+      "esId": "K-N_TW0Bg6Pnu4WYw-Ks",
+      "esIndex": "cnnoc_auth",
+      "host": "192.168.0.172",
+      "logLevel": "INFO",
+      "logType": "auth",
+      "message": "[2019-09-18 15:11:39.440] [auth] [192.168.0.172] [9001] [ykserver172] [Linux] [1.8.0_212] [main] [INFO] [springfox.documentation.spring.web.scanners.ApiListingReferenceScanner] [scan] [[Request mapping: menuVerifyName belongs to groups: [[ResourceGroup{groupName='menu-controller', position=0, controller=cn.cloudscope.basic.module.menu.controller.MenuController}]]",
+      "port": 9001,
+      "timestamp": "2019-09-18T07:11:39.440Z"
+}]
+*/
+var logShow = {
+    logs: [],
+    initData: function() {
+        var logArr = [];
+        for(var i = 0; i < 500; ++i) {
+            logArr.push({color: 'blue', id: i, text: '日志'+i});
+        }
+        for(var i = 501; i < 1000; ++i) {
+            logArr.push({color: 'red', id: i, text: '日志'+i})
+        }
+        for(var i = 1001; i < 1500; ++i) {
+            logArr.push({color: 'black', id: i, text: '日志'+i})
+        }
+        return logArr;
+    },
+    initLine: function(){
+        var html = '';
+        $.each(this.logs, function(i, o){
+            html += '<div class="line-item" style="color:'+o.color+'">_</div>';
+        });
+        $('.mod-line').html(html);
+    },
+    init: function(){
+        this.logs = this.initData();
+        this.initLine();
+    }
+};
