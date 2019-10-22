ef4b3bf2468013d8be2701d31dfa3403f92ba5ba
spinecat <157171828@qq.com>
2019-10-22 22:20:32 +0800

    重新复制文件内容，去掉了乱码；修改滑块高度为20px，样式为透明方框

--- scroll.js
+++ scroll.js
@@ -7,14 +7,18 @@ for (var i = 1; i <= 300; i++) {
     scrollContent.appendChild(row);
 }
  
-//指示鼠标左键是否处于按下状态的变量，在滑块上按下鼠标左键时设为true，在页面上任意位置松开时设回false
-//由于鼠标拖动滑块时可能会离开滑块，所以mouseup和mousemove事件是注册在window上的
+//指示鼠标左键是否处于按下状态的变量，
+//在滑块上按下鼠标左键时设为true，在页面上任意位置松开时设回false
+//由于鼠标拖动滑块时可能会离开滑块，
+//所以mouseup和mousemove事件是注册在window上的
 //在mousemove事件处理程序中，会检查该变量，以确定当前是否在拖动滑块
 var mouseHeld = false;
-//记录上一次mousemove事件发生时，鼠标的Y轴位置，每次发生mousemove事件时，跟上一次作比较，确定需要滚动多少距离
+//记录上一次mousemove事件发生时，鼠标的Y轴位置，每次发生mousemove事件时，
+//跟上一次作比较，确定需要滚动多少距离
 var previousClientY = 0;
-//滑块可滑动的距离，计算方式为整个滚动条高度度减去上下按钮的高度，再减去滑块本身的高度
-var barMoveLength = 300 - 20 * 2 - 50;
+//滑块可滑动的距离，计算方式为整个滚动条高度度减去上下按钮的高度，
+//再减去滑块本身的高度
+var barMoveLength = 300 - 20 * 2 - 20;
 //内容区域可滚动的距离，计算方式为内容区域的总高度减去内容区域本身的高度
 var contentMoveLength = scrollContent.scrollHeight - 300;
  
@@ -27,7 +31,8 @@ document.getElementById("btnDown").addEventListener("click", function () {
 });
  
 //为大幅度上下滚动点击区域注册事件处理程序
-//保存trackUp元素变量，因为每次滚动时，都要改变它的高度，以达到移动滑块的效果
+//保存trackUp元素变量，
+//因为每次滚动时，都要改变它的高度，以达到移动滑块的效果
 var trackUp = document.getElementById("trackUp");
 trackUp.addEventListener("click", function () {
     scrollToRelative(-300);
@@ -36,7 +41,8 @@ document.getElementById("trackDown").addEventListener("click", function () {
     scrollToRelative(300);
 });
  
-//为滑块注册鼠标按下事件处理程序，因为只有在滑块上按下鼠标左键时，才算开始拖动滑块
+//为滑块注册鼠标按下事件处理程序，
+//因为只有在滑块上按下鼠标左键时，才算开始拖动滑块
 document.getElementById("scrollBar").addEventListener("mousedown", function (e) {
     mouseHeld = true;
     previousClientY = e.clientY;
@@ -74,8 +80,10 @@ if (navigator.userAgent.indexOf("Firefox") < 0) {
  
 //确定内容区域当前是否在顶部或底部
 function isOnTopOrBottom() {
-    //判断是否在底部时，用了向上取整函数，因为在chrome下，滚动到底时，scrollTop常为小数，与contentMoveLength不等，向上取整之后一般相等
-    return scrollContent.scrollTop == 0 || Math.ceil(scrollContent.scrollTop) == contentMoveLength;
+    //判断是否在底部时，用了向上取整函数，因为在chrome下，滚动到底时，
+	//scrollTop常为小数，与contentMoveLength不等，向上取整之后一般相等
+    return scrollContent.scrollTop == 0 || 
+		Math.ceil(scrollContent.scrollTop) == contentMoveLength;
 }
  
 //鼠标滚轮事件的处理程序，relative为相对滚动距离，e为事件参数
@@ -83,8 +91,10 @@ function handleMouseWheel(relative, e) {
     //记录下滚动之前内容区域是否在两端
     var previousOnTopOrBottom = isOnTopOrBottom();
     scrollToRelative(relative);
-    //如果现在不在两端，或者现在在两端而滚动之前不在，则屏蔽默认滚轮行为————滚动整个页面
-    //反过来说，只有当“滚动”（实际上内容区域未滚动）前后内容区域都在某一端时，即已经到两端之后继续滚动时，才让滚动整个页面
+    //如果现在不在两端，或者现在在两端而滚动之前不在，
+	//则屏蔽默认滚轮行为————滚动整个页面
+    //反过来说，只有当“滚动”（实际上内容区域未滚动）前后内容区域都在某一端时，
+	//即已经到两端之后继续滚动时，才让滚动整个页面
     if (!isOnTopOrBottom() || (isOnTopOrBottom() && !previousOnTopOrBottom)) {
         e.preventDefault();
     }
@@ -101,8 +111,10 @@ function scrollTo(top) {
     }
  
     //设置滑块的位置，这是通过设置滑块上面的大幅度向上滚动点击区域的高度实现的
-    //滑块位置计算依据为滑块距顶部距离占总可滑动距离的比应与内容区域距顶部距离占总可滚动距离的比相等
-    var barDownDistance = scrollContent.scrollTop * barMoveLength / contentMoveLength;
+    //滑块位置计算依据为
+	//滑块距顶部距离占总可滑动距离的比 应与 内容区域距顶部距离占总可滚动距离的比 相等
+    var barDownDistance = 
+		scrollContent.scrollTop * barMoveLength / contentMoveLength;
     trackUp.style.height = barDownDistance + "px";
 }
  
